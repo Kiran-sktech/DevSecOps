2e807cdba68a8ba3c076697bd2acd906ea3eeb48
BhumikaDolnare01 <dolnarebhumika@gmail.com>
2026-09-11 23:27:02 +0530

    fix: skip external services during tests

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,10 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const connnectDB = async () => {
-  
-  mongoose.connection.on('connected', () => {
-    console.log('MongoDB connected');  
-  }) 
-await mongoose.connect(process.env.MONGODB_URI)}
 
-export default connnectDB;
+    if (process.env.NODE_ENV === 'test') {
+        return
+    }
+
+    mongoose.connection.on('connected', () => {
+        console.log('MongoDB connected')
+    })
+
+    await mongoose.connect(process.env.MONGODB_URI)
+}
+
+export default connnectDB
